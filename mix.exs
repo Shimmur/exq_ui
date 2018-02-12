1ea1ad731269b0a96e51a2929a528003d4f61b0c
Tomas Koci <koci24@uga.edu>
2018-02-12 13:05:26 -0500

    changed dependencies for exq

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule ExqUi.Mixfile do
   # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
     [
-      { :exq, "~> 0.9"},
+      {:exq, github: "Shimmur/exq", branch: "master"},
       { :plug, ">= 0.8.1 and < 2.0.0"},
       { :cowboy, "~> 1.0" },
       { :excoveralls, "~> 0.3", only: :test },
